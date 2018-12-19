027ffbb4eb7a5ada4e02546bbd6f7787f5932fb2
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-19 20:10:01 +0300

    Add movies view

--- stream/views.py
+++ stream/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def movies(request):
+    popular_movies_tmdb = tmdb.Movies('popular')
+    popular_movies = popular_movies_tmdb.info()['results']
+
+    upcoming_movies_tmdb = tmdb.Movies('upcoming')
+    upcoming_movies = upcoming_movies_tmdb.info()['results']
+
+    return render(request, 'movies.html', {'popular':popular_movies, 'upcoming':upcoming_movies})
