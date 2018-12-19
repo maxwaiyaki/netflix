1deb19a82f624a7e0cad78fabb918e4145207b0a
maxwaiyaki <maxwel.wairimu@gmail.com>
2018-12-19 20:12:35 +0300

    add movie and single movie views

--- stream/views.py
+++ stream/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import tmdbsimple as tmdb
+import json
+from time import mktime
+import time
+from datetime import datetime, date
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+#API KEYS and Request Parameters
+tmdb.API_KEY = '26731892a5e9d4990bdabe9dca773a49'
+DEVELOPER_KEY = 'AIzaSyCZKLBjdBQA7RIFL9uwy8z2BpDWeA0-p-s'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 
 # Create your views here.
 def movies(request):
@@ -17,7 +30,6 @@ def single_movie(request, movie_id):
     date_created_time_struct = time.strptime(date_created, '%Y-%m-%d')
     date_created_date = datetime.fromtimestamp(mktime(date_created_time_struct)).date()
     year = date_created_date.year
-    # Get movie name and use it to pass it as an argument to the youtube api.
     movie_name = movies['original_title']
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
     search_response = youtube.search().list(q=movie_name, part='id,snippet', maxResults=1).execute()
